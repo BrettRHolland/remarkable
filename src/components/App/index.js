94714553c9a7524b33768818c0963ade833eef8f
Brett Holland <brett.holland@gmail.com>
2020-12-04 10:47:40 -0500

    Update header and styling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,11 +8,12 @@ function App() {
 
   let conversionsBelowRoot = [];
   for (let x = rootSize; x >= rootSize - calculationsBelowRoot; x--) {
-    let conversion = x / rootSize;
+    let conversion =
+      Math.round((x / rootSize + Number.EPSILON) * 10000) / 10000;
     conversionsBelowRoot.unshift(
       <div className="conversion" key={x}>
-        <div className="px">{x}px</div>
-        <div className="rem">
+        <div className="conversion-px">{x}px</div>
+        <div className="conversion-rem">
           {Math.round((conversion + Number.EPSILON) * 10000) / 10000}rem
         </div>
       </div>
@@ -21,11 +22,12 @@ function App() {
 
   let conversionsAboveRoot = [];
   for (let x = rootSize + 1; x <= rootSize + calculationsAboveRoot; x++) {
-    let conversion = x / rootSize;
+    let conversion =
+      Math.round((x / rootSize + Number.EPSILON) * 10000) / 10000;
     conversionsAboveRoot.push(
       <div className="conversion" key={x}>
-        <div className="px">{x}px</div>
-        <div className="rem">
+        <div className="conversion-px">{x}px</div>
+        <div className="conversion-rem">
           {Math.round((conversion + Number.EPSILON) * 10000) / 10000}rem
         </div>
       </div>
@@ -35,20 +37,24 @@ function App() {
   return (
     <>
       <header>
-        <div className="root-size-container">
-          <div className="root-counter-container">
+        <h1 className="logo">
+          <strong>rem</strong>arkable
+        </h1>
+        <p className="description">simple px to rem conversion</p>
+        <div className="counter-container">
+          <div className="counter">
             <button
-              className="root-size-button"
+              className="counter-button"
               onClick={() => setRootSize(rootSize - 1)}
             >
               <i className="fas fa-chevron-left fa-fw" />
             </button>
-            <div className="root-size-number">
+            <div className="counter-number">
               {rootSize}
-              <span className="units">px</span>
+              <span className="counter-units">px</span>
             </div>
             <button
-              className="root-size-button"
+              className="counter-button"
               onClick={() => setRootSize(rootSize + 1)}
             >
               <i className="fas fa-chevron-right fa-fw" />
@@ -56,9 +62,9 @@ function App() {
           </div>
         </div>
       </header>
-      <div className="container">
+      <div className="conversions-container">
         <button
-          className="add-conversions-button"
+          className="conversions-button"
           onClick={() => {
             setCalculationsBelowRoot(calculationsBelowRoot + 5);
           }}
@@ -68,7 +74,7 @@ function App() {
         {conversionsBelowRoot}
         {conversionsAboveRoot}
         <button
-          className="add-conversions-button"
+          className="conversions-button"
           onClick={() => {
             setCalculationsAboveRoot(calculationsAboveRoot + 5);
           }}
